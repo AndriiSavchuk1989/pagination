8f7da2f478eb6d99927608573bc3ca85c7d9e804
Andrij <andrey.m.savchuk@gmail.com>
2019-05-22 00:37:23 +0300

    0002

--- src/index.js
+++ src/index.js
@@ -1,50 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import Pagination from "./Pagination";
-import Users from "./Users";
-
-import "./styles.css";
-
-const users = [
-  {
-    id: 1,
-    first_name: "Alan",
-    last_name: "Smith"
-  },
-  {
-    id: 2,
-    first_name: "Alan",
-    last_name: "Smith"
-  },
-  {
-    id: 3,
-    first_name: "Alan",
-    last_name: "Smith"
-  },
-  {
-    id: 4,
-    first_name: "Alan",
-    last_name: "Smith"
-  },
-  {
-    id: 5,
-    first_name: "Alan",
-    last_name: "Smith"
-  }
-];
 
 class Page extends React.Component {
   constructor(props) {
     super(props);
     this.state = { users: [] };
+    this.getAllUsers = this.getAllUsers.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    axios
+      .get("http://www.mocky.io/v2/5ce46dc53100007ea5742d31")
+      .then(response => console.log(response));
   }
   render() {
-    return (
-      <Pagination data={users}>
-        <Users />
-      </Pagination>
-    );
+    return <div />;
   }
 }
 
